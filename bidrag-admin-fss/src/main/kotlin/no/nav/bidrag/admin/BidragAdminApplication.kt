a1803adb82b10872b9cfc9063c2dc0d88096187a
Ugur Alpay Cenar <cenar93@gmail.com>
2025-04-17 09:45:43 +0200

    Initial commit

--- bidrag-admin-fss/src/main/kotlin/no/nav/bidrag/admin/BidragAdminApplication.kt
+++ bidrag-admin-fss/src/main/kotlin/no/nav/bidrag/admin/BidragAdminApplication.kt
@@ -1,9 +1,15 @@
 package no.nav.bidrag.admin
 
+import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.EnableAspectJAutoProxy
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [SecurityAutoConfiguration::class, ManagementWebSecurityAutoConfiguration::class])
+@EnableAspectJAutoProxy
+@EnableJwtTokenValidation(ignore = ["org.springframework", "org.springdoc"])
 class BidragAdminApplication
 
 fun main(args: Array<String>) {
