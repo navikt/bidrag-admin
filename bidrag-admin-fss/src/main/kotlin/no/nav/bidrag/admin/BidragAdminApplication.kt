cddf4b5c09f8df29aa93b36aa15f93f6518e66d7
Ugur Alpay Cenar <cenar93@gmail.com>
2026-05-22 15:02:15 +0200

    Oppgrader til SB4

--- bidrag-admin-fss/src/main/kotlin/no/nav/bidrag/admin/BidragAdminApplication.kt
+++ bidrag-admin-fss/src/main/kotlin/no/nav/bidrag/admin/BidragAdminApplication.kt
@@ -1,13 +1,22 @@
 package no.nav.bidrag.admin
 
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
-import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration
+import org.springframework.boot.security.autoconfigure.actuate.web.servlet.ManagementWebSecurityAutoConfiguration
+import org.springframework.boot.security.autoconfigure.web.servlet.ServletWebSecurityAutoConfiguration
 import org.springframework.context.annotation.EnableAspectJAutoProxy
 
-@SpringBootApplication(exclude = [SecurityAutoConfiguration::class, ManagementWebSecurityAutoConfiguration::class])
+@SpringBootApplication(
+    exclude = [
+        SecurityAutoConfiguration::class,
+        ManagementWebSecurityAutoConfiguration::class,
+        UserDetailsServiceAutoConfiguration::class,
+        ServletWebSecurityAutoConfiguration::class,
+    ],
+)
 @EnableAspectJAutoProxy
 @EnableJwtTokenValidation(ignore = ["org.springframework", "org.springdoc"])
 class BidragAdminApplication
